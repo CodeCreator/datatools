269d37cd9cdbedefb762e6c784420dff9ae3a810
CodeCreator <wettig.alexander@gmail.com>
2024-06-24 22:36:54 -0400

    More lenient requirements

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version='0.1',
     packages=['datatools'],
     install_requires=[
-        'tqdm==4.66.1',
-        'numpy==1.26.4',
-        'simple_parsing==0.1.5',
-        'mosaicml-streaming==0.7.5',
-        'datasets==2.18.0',
-        'sentencepiece==0.1.99',
+        'tqdm>=4.66.1',
+        'numpy>=1.26.4',
+        'simple_parsing>=0.1.5',
+        'mosaicml-streaming>=0.7.5',
+        'datasets>=2.18.0',
+        'sentencepiece>=0.1.99',
         # 'transformers==4.39.3',
     ],
     entry_points={
